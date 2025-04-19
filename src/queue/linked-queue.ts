1bad5bedf94a367d952081ae781970ea76e23072
Dan Ruan <danrsaquetti@gmail.com>
2025-04-19 11:49:54 -0300

    feat: Creating new data structure: LinkedQueue

---
+++ src/queue/linked-queue.ts
@@ -0,0 +1,32 @@
+import { IQueue } from './queue.interface'
+import { ILinkedList } from '../linked-lists/linked-list.interface'
+import { SinglyLinkedList } from '../linked-lists/singly-linked-list'
+
+export class LinkedQueue<T = any> implements IQueue<T> {
+
+  private linkedList: ILinkedList<T> = new SinglyLinkedList<T>()
+
+  enqueue(value: T) {
+    this.linkedList.insertLast(value)
+  }
+
+  dequeue() {
+    return this.linkedList.removeFirst()
+  }
+
+  first() {
+    return this.linkedList.first()
+  }
+
+  toArray() {
+    return this.linkedList.toArray()
+  }
+
+  isEmpty() {
+    return this.linkedList.isEmpty()
+  }
+
+  size() {
+    return this.linkedList.size()
+  }
+}
